4f90b5f2db364164cf3374914cefadb207bf5fd1
jifucha <jifucha@yjcp.com>
2018-03-21 10:56:37 +0800

    装饰器

--- day16/装饰器.py
+++ day16/装饰器.py
@@ -6,16 +6,21 @@ import time
 
 # 遵守开放封闭原则。
 
+
+def show_time(func):
+    def inner():
+        start = time.time()
+        func()
+        end = time.time()
+        print("spend %s" % (end - start))
+
+    return inner
+
+
+@show_time  # foo = show_time(foo)
 def foo():
     print("foo ...")
     time.sleep(1)
 
 
-def show_time(func):
-    start = time.time()
-    func()
-    end = time.time()
-    print("spend %s" % (end - start))
-
-
-show_time(foo)
+foo()
